cebe74f1a61a673a021797b6842b26cc374f3ddb
charmii22 <charmi.skylink@gmail.com>
2023-02-09 10:39:20 +0530

    OOPS demo completed

--- week2/OOPS/oops.js
+++ week2/OOPS/oops.js
@@ -1,63 +1,75 @@
+class Employee {
+  constructor(name, salary) {
+    this.name = name;
 
-// function createObj(){
-//     let obj = {}
-//     let property = document.getElementById('prop-input').value
-//     let value = document.getElementById('val-input').value
+    this.salary = 20000;
+  }
 
-//     obj[property] = value
-//     console.log(obj,property,value)
-//     document.getElementById('obj-output').innerHTML = JSON.stringify(obj)
-// }
-//oops
+  welcomeEmployee() {
+    console.log(`Welcome to our company`);
+    document.getElementById("message").innerHTML = `Welcome to our company`;
+  }
 
-//constructor
-class Employee{
-
-    constructor(name,address,salary){
-       this.name = name
-       this.address = address
-       this.salary = 20000
-    }
-
-    welcomeEmployee(){
-        console.log(`Welcome to our company` )
-       
-
-    }
-     
-    //encapsulation
-    setSalary(salary){
-        this.salary = salary
-    }
-
+  //encapsulation
+  setSalary(salary) {
+    this.salary = salary;
+  }
 }
 //abstraction
-Employee.prototype.displaySalary = function(){
-        console.log(`Salary is  ${this.salary}`)
-    
-}
+Employee.prototype.displaySalary = function () {
+  console.log(`Salary is  ${this.salary}`);
+
+  document.getElementById(
+    "salary-message"
+  ).innerHTML = `Salary is  ${this.salary}`;
+};
 //inheritance
-class Sales extends Employee{
-    constructor(name){
-        super(name)
-        this.name = name
-        
-    }
-    //polymorphism
-    //method overriding
-    welcomeEmployee(){
-        super.welcomeEmployee()
-        console.log(`Hope ${this.name} rises high in the sales department`);
-    
-    }
+class Sales extends Employee {
+  constructor(name) {
+    super(name);
+    this.name = name;
+  }
+  //polymorphism
+  //method overriding
+  welcomeEmployee() {
+    super.welcomeEmployee();
+    console.log(`Hope ${this.name} rises high in the sales department`);
+
+    document.getElementById(
+      "message"
+    ).innerHTML = `Hope ${this.name} rises high in the sales department`;
+  }
 }
+class Developer extends Employee {
+  constructor(name) {
+    super(name);
+    this.name = name;
+  }
+  //polymorphism
+  //method overriding
+  welcomeEmployee() {
+    super.welcomeEmployee();
+    console.log(`Hope ${this.name} rises high in the developer department`);
+    document.getElementById(
+      "message"
+    ).innerHTML = `Hope ${this.name} rises high in the developer department`;
+  }
+}
+function createEmployee() {
+  let empName = document.getElementById("emp-name").value;
+  let depName = document.getElementById("department").value;
 
-let emp1 = new Employee()
-emp1.displaySalary()
-let saleEmp = new Sales("abc")
-saleEmp.welcomeEmployee()
-saleEmp.setSalary("30000")
-saleEmp.displaySalary()
-
-
+  if (depName == "sales") {
+    let saleEmp = new Sales(empName);
+    saleEmp.setSalary("30000");
+    saleEmp.welcomeEmployee();
+    saleEmp.displaySalary();
+  }
+  if (depName == "developer") {
+    let devEmp = new Developer(empName);
+    devEmp.setSalary("40000");
+    devEmp.welcomeEmployee();
+    devEmp.displaySalary();
+  }
+}
 
